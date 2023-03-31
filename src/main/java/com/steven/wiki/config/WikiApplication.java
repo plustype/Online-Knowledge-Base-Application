ed6c98739e0ab3738fa86e80283b8b1a9e5cc026
yuzhao <zhaoyu.angel@gmail.com>
2023-03-31 14:29:35 -0700

    Develop Hello World API http://127.0.0.1:8080/hello

--- src/main/java/com/steven/wiki/config/WikiApplication.java
+++ src/main/java/com/steven/wiki/config/WikiApplication.java
@@ -1,11 +1,13 @@
-package com.steven.wiki;
+package com.steven.wiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+@ComponentScan("com.steven")
 @SpringBootApplication
 public class WikiApplication {
 
