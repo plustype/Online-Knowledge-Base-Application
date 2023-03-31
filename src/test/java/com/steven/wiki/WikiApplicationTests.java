bd748f840422b151d7eeeb69c1a1850846cc4f87
yuzhao <zhaoyu.angel@gmail.com>
2023-03-31 00:20:57 -0700

    init

---
+++ src/test/java/com/steven/wiki/WikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steven.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
