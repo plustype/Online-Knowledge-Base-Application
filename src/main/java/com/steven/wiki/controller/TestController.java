eb084bc472cae1337c73d432c34e84e258712ebc
yuzhao <zhaoyu.angel@gmail.com>
2023-03-31 15:06:47 -0700

    Add helloPost Add hello API requestTest.http

--- src/main/java/com/steven/wiki/controller/TestController.java
+++ src/main/java/com/steven/wiki/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.steven.wiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,10 @@ public class TestController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello World";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello, " + name;
     }
 }
