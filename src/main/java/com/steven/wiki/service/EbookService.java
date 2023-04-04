676dcdd80a6de68709c79f86ace87342bf57a6c9
yuzhao <zhaoyu.angel@gmail.com>
2023-04-04 16:14:48 -0700

    Create a CopyUtil class for warping BeanUtils (copying properties)

--- src/main/java/com/steven/wiki/service/EbookService.java
+++ src/main/java/com/steven/wiki/service/EbookService.java
@@ -1,15 +1,13 @@
 package com.steven.wiki.service;
 
-import com.steven.wiki.domain.Ebook;
 import com.steven.wiki.domain.EbookExample;
 import com.steven.wiki.mapper.EbookMapper;
 import com.steven.wiki.request.EbookReq;
 import com.steven.wiki.response.EbookResp;
+import com.steven.wiki.utils.CopyUtil;
 import jakarta.annotation.Resource;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,21 +21,21 @@ public class EbookService {
         //fix, create criteria(where in sql); 固定写法，创建criteria变量（相当于sql中的where条件）
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
-
         criteria.andNameLike("%" + req.getName() + "%");
+
         var ebookList = ebookMapper.selectByExample(ebookExample);
         
         /*
         transfer properties to EbookResp; 将值传给封装的返回值类
          */
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-            respList.add(ebookResp);
-        }
+//        List<EbookResp> respList = new ArrayList<>();
+//        for (Ebook ebook : ebookList) {
+//            EbookResp ebookResp = new EbookResp();
+//            BeanUtils.copyProperties(ebook, ebookResp);
+//            respList.add(ebookResp);
+//        }
 
-        return  respList;
+        return CopyUtil.copyList(ebookList, EbookResp.class); //copy list value
     }
 
 }
