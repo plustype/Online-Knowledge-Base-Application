0f9138b22b7327454637324efb7884ea3329c728
yuzhao <zhaoyu.steven@gmail.com>
2023-04-11 15:25:47 -0700

    Vue multi-environment setup. Add dev and prod config Add dev and prod config in npm-build (package.json)

--- web/src/main.ts
+++ web/src/main.ts
@@ -15,3 +15,6 @@ const icons: any = Icons;
 for (const i in icons) {
     app.component(i, icons[i]);
 }
+
+console.log('Env: ', process.env.NODE_ENV);
+console.log('Server: ', process.env.VUE_APP_SERVER);
