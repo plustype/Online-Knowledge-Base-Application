e7cc6f20b86c7a84cfdb0d0fa52f5646407de91c
yuzhao <zhaoyu.steven@gmail.com>
2023-04-11 13:13:05 -0700

    Import icons, npm install --save @ant-design/icons-vue

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -48,10 +48,38 @@
     <a-layout-content
       :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
   >
-      <pre>
-        {{ebooks}}  <!--使用{{xxx}}来获取变量 -->
-        {{ebooks2}}
-      </pre>
+      <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+        <template #footer>
+          <div>
+            <b>ant design vue</b>
+            footer part
+          </div>
+        </template>
+        <template #renderItem="{ item }">
+          <a-list-item key="item.title">
+            <template #actions>
+          <span v-for="{ type, text } in actions" :key="type">
+            <component v-bind:is="type" style="margin-right: 8px" />
+            {{ text }}
+          </span>
+            </template>
+            <template #extra>
+              <img
+                  width="272"
+                  alt="logo"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+              />
+            </template>
+            <a-list-item-meta :description="item.description">
+              <template #title>
+                <a :href="item.href">{{ item.title }}</a>
+              </template>
+              <template #avatar><a-avatar :src="item.avatar" /></template>
+            </a-list-item-meta>
+            {{ item.content }}
+          </a-list-item>
+        </template>
+      </a-list>
   </a-layout-content>
   </a-layout>
 </template>
@@ -60,6 +88,20 @@
 import { defineComponent, onMounted, ref, reactive, toRef } from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 import axios from 'axios';
+
+const listData: Record<string, string>[] = [];
+
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://www.antdv.com/',
+    title: `ant design vue part ${i}`,
+    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    description:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
 
 export default defineComponent({
   name: 'Home',
@@ -70,6 +112,17 @@ export default defineComponent({
     console.log("setup");
     const ebooks = ref(); //定义响应式数据变量，变化实时响应到界面
     const ebooks1 = reactive({books: []}); //books是自己定义的属性，属性值用来放电子书列表
+    const pagination = {
+      onChange: (page: number) => {
+        console.log(page);
+      },
+      pageSize: 3,
+    };
+    const actions: Record<string, string>[] = [
+      { type: 'StarOutlined', text: '156' },
+      { type: 'LikeOutlined', text: '156' },
+      { type: 'MessageOutlined', text: '2' },
+    ];
 
     onMounted(() => {   //生命周期函数
       console.log("onMounted");
@@ -83,7 +136,10 @@ export default defineComponent({
 
     return {
       ebooks,  //html代码要拿到响应式变量，需要在setup最后return
-      ebooks2: toRef(ebooks1, "books") //将ebooks1里面的books属性编程响应式变量
+      ebooks2: toRef(ebooks1, "books"), //将ebooks1里面的books属性编程响应式变量
+      listData,
+      pagination,
+      actions,
     }
   }
 });
