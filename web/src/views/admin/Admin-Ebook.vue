7e9f9629f695e4e1f6fde677754d006a3301a784
yuzhao <zhaoyu.steven@gmail.com>
2023-04-17 11:54:10 -0700

    Modify the layout of Admin-Ebook.vue

--- web/src/views/admin/Admin-Ebook.vue
+++ web/src/views/admin/Admin-Ebook.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="about">
-    <h1>This is an Ebook Management page</h1>
-  </div>
+  <a-layout>
+    <a-layout-content
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+    >
+      <div class="about">
+        <h1>This is an Ebook Management page1111</h1>
+      </div>
+    </a-layout-content>
+  </a-layout>
 </template>
