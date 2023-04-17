91364f60e8268a93f58508420adcc820b1a59997
yuzhao <zhaoyu.steven@gmail.com>
2023-04-17 11:10:12 -0700

    Add Admin-Ebook.vue page New page dev: Admin-Ebook.vue, index.ts, TheHeader.vue

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import AdminEbook from '../views/admin/Admin-Ebook.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -10,11 +12,17 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/about',
     name: 'About',
+    component: About
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/admin/ebook',
+    name: 'AdminEbook',
+    component: AdminEbook
+  },
 ]
 
 const router = createRouter({
