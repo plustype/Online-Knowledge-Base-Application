f8c70660335910a544debdeffae23611e48251e0
yuzhao <zhaoyu.steven@gmail.com>
2023-04-17 10:40:22 -0700

    Add AOP. Print API elapsed time, request info and return value Remove filter and interceptor

--- src/main/java/com/steven/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/steven/wiki/config/SpringMvcConfig.java
@@ -1,19 +1,19 @@
-package com.steven.wiki.config;
-
-import com.steven.wiki.interceptor.LogInterceptor;
-import jakarta.annotation.Resource;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-    @Resource
-    LogInterceptor loginInterceptor;
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginInterceptor)
-                .addPathPatterns("/**");
-    }
-}
+//package com.steven.wiki.config;
+//
+//import com.steven.wiki.interceptor.LogInterceptor;
+//import jakarta.annotation.Resource;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
+//    @Resource
+//    LogInterceptor loginInterceptor;
+//
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(loginInterceptor)
+//                .addPathPatterns("/**");
+//    }
+//}
