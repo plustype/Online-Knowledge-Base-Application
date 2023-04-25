8dae0f4c899919c02ef0361ca2d5061b5f183c32
yuzhao <zhaoyu.steven@gmail.com>
2023-04-25 15:52:40 -0700

    Back-end: generate Mybatis persistence layer: category basic unlimited tree structure: id + parent

--- doc/all.sql
+++ doc/all.sql
@@ -36,3 +36,32 @@ insert into `ebook` (id, name, description) values (1, 'Head First Java', 'A Bra
 insert into `ebook` (id, name, description) values (2, 'Java Advance', 'The advance topics for Java application');
 insert into `ebook` (id, name, description) values (3, 'Spring Boot Basic', 'Learn Spring Boot with zero experience');
 insert into `ebook` (id, name, description) values (4, 'Data Analysis', 'Learn data analysis with Python');
+
+#-----Category table-------
+drop table if exists `category`;
+create table `category`
+(
+    `id`     bigint      not null,
+    `parent` bigint      not null default 0,
+    `name`   varchar(50) not null,
+    `sort`   int,
+    primary key (`id`)
+) engine = innodb default charset = utf8mb4;
+
+insert into `category` (id, parent, name, sort) VALUES (1000, 0000, 'Front-End Dev', 1000);
+insert into `category` (id, parent, name, sort) VALUES (1001, 1000, 'Vue', 1001);
+insert into `category` (id, parent, name, sort) VALUES (1002, 1000, 'HTML & CSS', 1002);
+insert into `category` (id, parent, name, sort) VALUES (1003, 1000, 'React', 1003);
+insert into `category` (id, parent, name, sort) VALUES (2000, 0000, 'Java', 2000);
+insert into `category` (id, parent, name, sort) VALUES (2001, 2000, 'Java Basic', 2001);
+insert into `category` (id, parent, name, sort) VALUES (2002, 2000, 'Java Frame', 2002);
+insert into `category` (id, parent, name, sort) VALUES (3000, 3000, 'Database', 3000);
+insert into `category` (id, parent, name, sort) VALUES (3001, 3000, 'MySQL', 3001);
+insert into `category` (id, parent, name, sort) VALUES (4000, 4000, 'Python', 4000);
+insert into `category` (id, parent, name, sort) VALUES (4001, 4000, 'Python Basic', 4001);
+insert into `category` (id, parent, name, sort) VALUES (4002, 4000, 'Python Advance', 4002);
+insert into `category` (id, parent, name, sort) VALUES (5000, 5000, 'Server', 5000);
+insert into `category` (id, parent, name, sort) VALUES (5001, 5000, 'AWS', 5001);
+insert into `category` (id, parent, name, sort) VALUES (6000, 6000, 'Data Structure', 6000);
+insert into `category` (id, parent, name, sort) VALUES (6001, 6000, 'Array', 6001);
+insert into `category` (id, parent, name, sort) VALUES (6002, 6000, 'Hash Table', 6002);
